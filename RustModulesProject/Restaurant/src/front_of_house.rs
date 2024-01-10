7bda335c2b5eaaed70997f09e08e34b9ba5e803d
Bhanu Rana <mrbeast@Bhanus-MacBook-Air.local>
2024-01-10 18:08:57 +0530

    Rust Module System

---
+++ RustModulesProject/Restaurant/src/front_of_house.rs
@@ -0,0 +1,2 @@
+// We can also have a child module, but to have a child module we need to create a new folder with the same name as the parent module and create the child module there
+pub mod hosting;
---
+++ RustModulesProject/Restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,4 @@
+pub fn add_to_waitlist()->String{
+
+return String::from("Funk You!")
+}
